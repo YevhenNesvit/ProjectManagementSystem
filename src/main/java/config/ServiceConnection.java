18ed10fc8e2ecce64adcfe38facad5738518c4c2
Yevhen <yevhen.nesvit@gmail.com>
2022-09-20 12:11:40 +0300

    Fix Multiple DatabaseManagerConnector creation

--- src/main/java/config/ServiceConnection.java
+++ src/main/java/config/ServiceConnection.java
@@ -1,11 +1,13 @@
 package config;
 
 public class ServiceConnection {
+    private final String dbPassword = System.getenv("dbPassword");
+    private final String dbUsername = System.getenv("dbUsername");
+    DatabaseManagerConnector connector = new DatabaseManagerConnector((new PropertiesConfig().loadProperties("application.properties")),
+            dbUsername, dbPassword);
 
     public DatabaseManagerConnector connect() {
-        String dbPassword = System.getenv("dbPassword");
-        String dbUsername = System.getenv("dbUsername");
-        return new DatabaseManagerConnector(new PropertiesConfig().loadProperties("application.properties"),
-                dbUsername, dbPassword);
+
+        return connector;
     }
 }
